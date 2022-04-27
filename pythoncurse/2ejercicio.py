9ceaa0b2b49c99460e2f91d4a6717b0fca09a4df
Rodrigo_LaptopI3_600U <rodrigogutierrez273@gmail.com>
2022-04-27 15:28:28 -0300

    ejercicio 3

---
+++ pythoncurse/2ejercicio.py
@@ -0,0 +1,8 @@
+from operator import le
+
+
+let = "Hola Mundo!"
+print(let)
+
+let2= "“SCRIPT“ = “PROGRAMA”"
+print(let2.lower())
---
+++ pythoncurse/3ejercicio.py
@@ -0,0 +1,4 @@
+let ="mi nombre es rodrigo y estoy creando muchos archivos"
+
+print(let)
+print(type(let))
